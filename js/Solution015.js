b0045964ed438146ab6920263f9a71be38e0ecde
Agustin Maya <a_maya@outlook.com>
2022-05-24 23:26:57 -0400

    Solution015 - You Can't Code Under Pressure #1

---
+++ js/Solution015.js
@@ -0,0 +1,18 @@
+//PREP
+//We will receive an integer
+//We will have tu return the double of that integer
+//The integer can be positive, negative or 0
+
+//function
+function doubleInteger(i){
+//double the integer
+    i *= 2;
+//return the result
+    return i;
+}
+
+console.log(doubleInteger(2));  //output: 4
+console.log(doubleInteger(-2));  //output: -4
+console.log(doubleInteger(0));  //output: 0
+console.log(doubleInteger(10));  //output: 20
+
