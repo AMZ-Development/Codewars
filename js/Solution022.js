4d0bdda3d888fffb97b4751fd3ff5b5439928f7c
Agustin Maya <a_maya@outlook.com>
2022-05-31 23:46:29 -0400

    Soultion022 - Sum of Differences

---
+++ js/Solution022.js
@@ -0,0 +1,19 @@
+//PREP
+//We will recieve an array
+//we have to return the sum of the differences between consecutive pairs in descending order
+//First, make the array go in descending order
+//get the difference between the pairs
+//return the sum of the differences
+
+function sumOfDifferences(arr){
+    //make array go in descending order 
+    arr.sort((a, b) => b - a);
+    //If we just take the difference of the highest number and the lowest number we will get the same result
+    //as if we did the sum of the differences
+    //return the difference of the highest number and lowest number
+    return arr[0] - arr[arr.length - 1];
+}
+
+//TEST
+console.log(sumOfDifferences([2,1,10])); //output: 9
+console.log(sumOfDifferences([-3,-2,-1])); //output: 2
