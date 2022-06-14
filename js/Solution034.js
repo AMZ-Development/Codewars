6d95c603473ab1c78dfaa4bbb79be445705bc7fd
Agustin Maya <a_maya@outlook.com>
2022-06-13 23:46:46 -0400

    Solution034 - Function 2 - Squaring an Argument

---
+++ js/Solution034.js
@@ -0,0 +1,11 @@
+//PREP
+//We will receive a number
+//Return the square of that number
+
+
+const square = (n) => Math.pow(n, 2);
+
+//test
+console.log(square(4)); //Output: 16
+console.log(square(-2)); //Output: 4
+console.log(square(5)); //Output: 25
