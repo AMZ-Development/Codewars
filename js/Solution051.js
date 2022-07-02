790416ae98d9e35ae77efb325d42453b4edb9ca2
Agustin Maya <a_maya@outlook.com>
2022-07-01 23:41:40 -0400

    Solution 051 - Opposite number

---
+++ js/Solution051.js
@@ -0,0 +1,12 @@
+//PREP
+//We will receive an integer
+//We have to return the opposite of that number(if negative return positive, or viceversa)
+
+
+let opposite = number => number * -1
+
+//TESTS
+console.log(opposite(1)); //Output: -1
+console.log(opposite(45)); //Output: -45
+console.log(opposite(-10)); //Output: 10
+console.log(opposite(-21)); //Output: 21
