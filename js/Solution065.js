99e3cb95c3ff365ce91743d42382a455bcf624aa
Agustin Maya <a_maya@outlook.com>
2022-07-18 22:00:09 -0400

    Solution065 - Convert a Number to a String!

---
+++ js/Solution065.js
@@ -0,0 +1,13 @@
+//PREP
+
+//We will receive a number, it can be integer or float
+//The number should be converted and return as a string
+
+function numberToString(num) {
+    return num.toString();
+}
+
+//Tests
+console.log(numberToString(23)); //Output: '23'
+console.log(numberToString(0)); //Output: '0'
+console.log(numberToString(-23)); //Output: '-23'
